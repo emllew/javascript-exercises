3f0a5203fd5fb9047fcc8854d618df1770755800
Emily <emilyllewelyn@gmail.com>
2021-02-16 17:33:45 +0000

    test repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (text, n) {
+  if (n < 0) {
+    return 'ERROR';
+  }
+  let string = ''
+  for (i = 0; i < n; i++) {
+    string += text
+  }
+  return string;
 }
 
 module.exports = repeatString
